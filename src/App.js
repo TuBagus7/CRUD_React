7eefc5cfe10b0b54933726cc968f5a53e85ee876
Tu Bagus Dwi Fikri <tubagusoseng@gmail.com>
2023-08-23 01:53:03 +0700

    Crud_React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+// Ini adalah Komponen Utama
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState("");
+  var [userUpdate, setuserUpdate] = useState("");
+  var [namaUserLama, setnamaUserLama] = useState("");
+  function ubahUser(e) {
+    setUser(e.target.value);
+    // console.log(e.target.value);
+  }
+  function tambahData() {
+    setUsers([...users, user]);
+    setUser("");
+  }
+
+  function hapusData(nama) {
+    const userBaru = users.filter((nama_user) => nama_user !== nama);
+    setUsers(userBaru);
+  }
+
+  function editData(nama) {
+    setuserUpdate(nama);
+    setnamaUserLama(nama);
+  }
+
+  function updateData() {
+    var usersSelainDiPilih = users.filter(
+      (nama_user) => nama_user !== namaUserLama
+    );
+    usersSelainDiPilih.push(userUpdate);
+    setUsers(usersSelainDiPilih);
+    setuserUpdate("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={user} type="text" onChange={(e) => ubahUser(e)} />
+      <button onClick={tambahData}>Tambah</button>
+      <br />
+      <input
+        value={userUpdate}
+        type="text"
+        onChange={(e) => setuserUpdate(e.target.value)}
+      />
+      <button onClick={updateData}>Update</button>
+      <br />
+      <h4>Daftar users</h4>
+      <ul>
+        {users.map((nama, i) => (
+          <li key={i}>
+            {nama} <button onClick={() => hapusData(nama)}>Delete</button>
+            <button onClick={() => editData(nama)}>Edit</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// Ini adalah file testing dari app,
+// jadi di dalam pembuatan project  ada namanya tahap development dan prodaction
+//sebelum masuk ke tahap production, seharunya ada testing testing terlebih dahulu
